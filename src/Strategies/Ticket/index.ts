6cec12f1d153824b34e51ddda9e358e5f0a9051a
roll1226 <tomo_yasu_4724@icloud.com>
2024-05-16 14:14:57 +0900

    add: TSdoc

--- src/Strategies/Ticket/index.ts
+++ src/Strategies/Ticket/index.ts
@@ -4,14 +4,40 @@ export interface TicketStrategy {
   isAvailableChildAttraction(): boolean;
 }
 
+/**
+ * @export getPrice
+ * @class Ticket
+ */
 export class Ticket {
+  /**
+   * @private
+   * @type {TicketStrategy}
+   * @memberof Ticket
+   */
   private _strategy: TicketStrategy;
+  /**
+   * Creates an instance of Ticket.
+   * @param {TicketStrategy} strategy
+   * @memberof Ticket
+   */
   constructor(strategy: TicketStrategy) {
     this._strategy = strategy;
   }
 
+  /**
+   * @returns number
+   * @memberof Ticket
+   */
   getPrice = (): number => this._strategy.getPrice();
+  /**
+   * @returns number
+   * @memberof Ticket
+   */
   getUserCount = (): number => this._strategy.getUserCount();
+  /**
+   * @returns boolean
+   * @memberof Ticket
+   */
   isAvailableChildAttraction = (): boolean =>
     this._strategy.isAvailableChildAttraction();
 }
