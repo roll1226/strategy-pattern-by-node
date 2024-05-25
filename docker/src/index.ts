5f48e497f16c21e01894162630a03d5ea69384af
roll1226 <tomo_yasu_4724@icloud.com>
2024-05-25 13:08:12 +0900

    feat: post Method

--- docker/src/index.ts
+++ docker/src/index.ts
@@ -5,13 +5,27 @@ const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
 
-app.post("/users", async (req, res) => {
+app.get("/user/create", (req, res) => {
+  res.send(`
+    <form action="/user/create" method="post">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email"><br><br>
+        <button type="submit">Submit</button>
+      </form>
+  `);
+});
+
+app.post("/user/create", async (req, res) => {
+  console.log(req.body);
   const { name, email } = req.body;
   const user = await prisma.user.create({
     data: { name, email },
